2091fd3a4273e244ce13d67004f83277d4e9ec31
kikicod3r <kitajjenkins@gmail.com>
2024-03-23 19:37:35 -0500

    Added additional context for AI

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function suggestSong(event) {
   event.preventDefault();
   let userInput = document.querySelector("#user-mood");
   let apiKey = "1687841bccf274a08foafbat6c3f1ff0";
-  let prompt = `Please suggest a song including the name of the artist to fit the mood the user is currently feeling from the word ${userInput.value} in basic HTML, separating each line with a <br />.`;
+  let prompt = `Please suggest a song including the name of the artist to fit the mood the user is currently feeling from the word ${userInput.value} in basic HTML, separating each sentence with a <br /> element.`;
   let context =
     "You are well versed in all music genres and can suggest a song based off of the user's mood. You know a variety of genres including pop, rock, rap, r&b, country, hip hop, and electronic.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
