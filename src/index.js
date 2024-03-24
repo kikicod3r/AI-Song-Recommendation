7d55059c9406cd85b7e5fc85241eb42c1a122db0
kikicod3r <kitajjenkins@gmail.com>
2024-03-23 19:27:12 -0500

    Added AI API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function suggestSong(event) {
-  event.preventDefault();
-
+function showSong(response) {
   new Typewriter("#song-suggestion", {
-    strings: "My favorite song right now is Jaded by Miley Cyrus",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
-    delay: natural,
+    delay: 100,
   });
+}
+
+function suggestSong(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#user-mood");
+  let apiKey = "1687841bccf274a08foafbat6c3f1ff0";
+  let prompt = `Please suggest a song including the name of the artist to fit the mood the user is currently feeling from the word ${userInput.value} in basic HTML, separating each line with a <br />.`;
+  let context =
+    "You are well versed in all music genres and can suggest a song based off of the user's mood. You know a variety of genres including pop, rock, rap, r&b, country, hip hop, and electronic.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(showSong);
 }
 
 let songButtonElement = document.querySelector("#song-button");
