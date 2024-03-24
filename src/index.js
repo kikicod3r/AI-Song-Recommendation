0d840e8b1936a3a4b5d95d8395288ef4e0c1505b
kikicod3r <kitajjenkins@gmail.com>
2024-03-24 12:09:46 -0500

    Improved UX
    CSS animation, note while AI is generating song, added more detail to AI context

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function showSong(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: "",
-    delay: 100,
+    delay: 50,
   });
 }
 
@@ -13,12 +13,12 @@ function suggestSong(event) {
   let apiKey = "1687841bccf274a08foafbat6c3f1ff0";
   let prompt = `Please suggest a song including the name of the artist to fit the mood the user is currently feeling from the word ${userInput.value} in basic HTML, separating each sentence with a <br /> element.`;
   let context =
-    "You are well versed in all music genres and can suggest a song based off of the user's mood. You know a variety of genres including pop, rock, rap, r&b, country, hip hop, and electronic.";
+    "You are well versed in all music genres and can suggest a song based off of the user's mood. You know a variety of genres including pop, rock, rap, r&b, country, hip hop, and electronic. Your song suggestions should consider all music genres and be unique to the user.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let songResponseElement = document.querySelector("#song-suggestion");
+  songResponseElement.classList.remove("hidden");
+  songResponseElement.innerHTML = `<div class="generating"> ⌛Choosing a ${userInput.value} feeling song for you...</div>`;
 
   axios.get(apiUrl).then(showSong);
 }
