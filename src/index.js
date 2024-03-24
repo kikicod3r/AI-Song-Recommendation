cdf2ca5b822f5ac6558fc6a013b2369895376801
kikicod3r <kitajjenkins@gmail.com>
2024-03-24 12:44:09 -0500

    Styling updates
    Gradient background and font

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function suggestSong(event) {
   event.preventDefault();
   let userInput = document.querySelector("#user-mood");
   let apiKey = "1687841bccf274a08foafbat6c3f1ff0";
-  let prompt = `Please suggest a song including the name of the artist to fit the mood the user is currently feeling from the word ${userInput.value} in basic HTML, separating each sentence with a <br /> element.`;
+  let prompt = `Please suggest a song including the name of the artist to fit the mood the user is currently feeling from the word ${userInput.value}.`;
   let context =
     "You are well versed in all music genres and can suggest a song based off of the user's mood. You know a variety of genres including pop, rock, rap, r&b, country, hip hop, and electronic. Your song suggestions should consider all music genres and be unique to the user.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
